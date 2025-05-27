3c774d522ddacc80163a6e6fc072fc5b2462f573
Amritha07dec <amritha07dec@gmail.com>
2025-05-27 10:54:40 +0530

    label encoding

--- model_lstm.py
+++ model_lstm.py
@@ -7,6 +7,7 @@ from data_preprocessing.delete import time_series_list, labels
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
@@ -14,6 +15,21 @@ print("Using device:", device)
 # Prepare data
 padded_samples = pad_samples(time_series_list, target_dim=6)
 X_train, X_val, y_train, y_val = split_data(padded_samples, labels)
+print("Train labels:", np.unique(y_train))
+print("Val labels:", np.unique(y_val))
+#The labels range has to be [0,3]
+y_train = [label - 1 for label in y_train]
+y_val = [label - 1 for label in y_val]
+
+# After splitting
+#X_train, X_val, y_train, y_val = split_data(padded_samples, labels)
+
+# ✅ Add this to inspect unique label values
+
+print("Train labels(encoded):", np.unique(y_train))
+print("Val labels(encoded):", np.unique(y_val))
+
+
 
 train_dataset = TimeSeriesDataset(X_train, y_train)
 val_dataset = TimeSeriesDataset(X_val, y_val)
@@ -28,7 +44,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # Train model
-train(model, train_loader, val_loader, criterion, optimizer, epochs=20)
+train(model, train_loader, val_loader, criterion, optimizer, epochs=40)
 
 # Save model
 torch.save(model.state_dict(), "lstm_model.pth")
