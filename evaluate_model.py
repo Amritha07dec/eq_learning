5d63f41e3abab317bde737d66c014abf65268894
Amritha07dec <amritha07dec@gmail.com>
2025-05-27 10:52:17 +0530

    push changes

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,19 +3,20 @@ from train import LSTMClassifier,Conv1DLSTMClassifier, pad_samples, split_data, 
 from data_preprocessing.delete import time_series_list, labels
 import torch
 from torch.utils.data import DataLoader
+from split import X_val, y_val
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Prepare validation data
 padded_samples = pad_samples(time_series_list, target_dim=6)
-_, X_val, _, y_val = split_data(padded_samples, labels)
+#_, X_val, _, y_val = split_data(padded_samples, labels)
 
 val_dataset = TimeSeriesDataset(X_val, y_val)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
 # Load model
 model = Conv1DLSTMClassifier()
-#model.load_state_dict(torch.load("Conv1DLSTM_model.pth", map_location=device))
+model.load_state_dict(torch.load("Conv1DLSTM_model.pth", map_location=device))
 model.to(device)
 
 # Plot confusion matrix
